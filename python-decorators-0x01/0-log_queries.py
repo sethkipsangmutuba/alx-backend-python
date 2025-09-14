d060178fd500d0bca7098b12ef10c2d61eff2ac3
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-09-14 22:23:36 +0300

    Create 0-log_queries.py

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sqlite3
+import functools
+
+def log_queries():
+    """Decorator that logs SQL queries"""
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            query = kwargs.get("query")
+            if query is None and args:
+                query = args[0]
+            if query:
+                print(f"[LOG] Executing query: {query}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@log_queries()
+def fetch_all_users(query):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+fetch_all_users(query="SELECT * FROM users")
