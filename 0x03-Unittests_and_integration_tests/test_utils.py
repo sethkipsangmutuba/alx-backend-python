b4e43e4067229f4321ba8acb2382d144bd6aefb3
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-09-21 22:42:05 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Unit tests for utils.access_nested_map
-"""
+"""Unit tests for utils.py"""
 
 import unittest
 from parameterized import parameterized
@@ -9,7 +7,7 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Tests for the access_nested_map function"""
+    """Test access_nested_map function."""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -17,5 +15,14 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Test access_nested_map returns the expected result"""
+        """Test that access_nested_map returns expected result."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test that KeyError is raised for invalid path."""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
