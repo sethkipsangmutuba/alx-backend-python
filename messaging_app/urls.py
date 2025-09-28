86673127cadeafd2797ad3bc5e83932ae764addd
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-09-28 22:45:58 +0300

    Update urls.py

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from chats.views import MessageViewSet
-from chats import auth as chat_auth
-
-router = DefaultRouter()
-router.register(r'messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('', include(chat_auth.urlpatterns)),
+    path('api/auth/', include('chats.auth')),  # JWT endpoints
+    path('api/chats/', include('chats.urls')),  # your chat endpoints
 ]
