bc71676d7b0125bf3b91e2125578272a81621cac
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-09-28 22:15:58 +0300

    Create views.py

---
+++ chats/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+from .models import Message
+from .serializers import MessageSerializer
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
