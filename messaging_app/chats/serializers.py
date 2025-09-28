8710b5a8e44b94365e39288517728fd99a48d222
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-09-28 22:44:26 +0300

    Delete messaging_app/chats directory

--- messaging_app/chats/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Message
-
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = ['id', 'user', 'content', 'created_at']
-        read_only_fields = ['user', 'created_at']
