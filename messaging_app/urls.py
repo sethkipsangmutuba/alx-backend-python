6c7d66f1e4221b186c08ff6dcd90d0cf51f76c38
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-10-05 21:24:41 +0300

    Delete messaging_app directory

--- messaging_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('chats.auth')),  # JWT endpoints
-    path('api/chats/', include('chats.urls')),  # your chat endpoints
-]
